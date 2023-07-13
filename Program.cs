f60d313b75741d5b231d5fb61f706cbf53ce74b4
nochnoibriz@gmail.com <nochnoibriz@gmail.com>
2023-07-13 14:38:12 +0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using static System.Net.WebRequestMethods;
+
+public class MyClass
+{
+    public int ID { get; set; } = -1; 
+    protected string Name { get; set; } = "Unknown"; 
+
+    public virtual string GetText() 
+    {
+    return (ID  + " " + Name); 
+    }
+}
