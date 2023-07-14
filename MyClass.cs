024b81cdc0cb1935b08b7974360923f2092bcbac
Vladimir <nochnoibriz@gmail.com>
2023-07-14 09:11:53 +0600

    myClass added

---
+++ MyClass.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassConsoleFacktorial
+{
+    public class MyClass
+    {
+        public int ID { get; set; } = -1;
+        protected string Name { get; set; } = "Unknown";
+
+        public virtual string GetText()
+        {
+            return (ID + " " + Name);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,12 +1,2 @@
 ﻿using static System.Net.WebRequestMethods;
 
-public class MyClass
-{
-    public int ID { get; set; } = -1; 
-    protected string Name { get; set; } = "Unknown"; 
-
-    public virtual string GetText() 
-    {
-    return (ID  + " " + Name); 
-    }
-}
